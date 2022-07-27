d450fd4e828ba00df56867a02ffa30fe31dc8144
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-27 11:21:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import About from './component/About/About';
+import { createContext } from 'react';
+import './App.css';
+import AboutMyself from './component/AboutMyself';
+// import About from './component/About/About';
+import AboutMeState from './component/Note/contextApi/AboutMeState';
 import NoteState from './component/Note/contextApi/NoteState';
+import Component1 from './component/Component1';
 
+export const FirstName = createContext();
+export const LastName = createContext();
 
 function App() {
-    
+
+
   return (
+    <>
+      <FirstName.Provider value={'Aditya'}>
+        <LastName.Provider value={"Kumar"} >
+          <Component1 />
+        </LastName.Provider>
+      </FirstName.Provider>
+
+      {/* <Component1 /> */}
       <NoteState>
-          <div className="app">
-              <h1>hello world</h1>
-              <About />
-          </div>
+        {/* <div className="app"> */}
+          {/* <h1>hello world</h1> */}
+          {/* <About /> */}
+        <AboutMeState>
+          <AboutMyself />
+        </AboutMeState>
+
+
+        {/* </div> */}
+
       </NoteState>
-    );
-};
+    </>
+  );
+}
 
 export default App;
